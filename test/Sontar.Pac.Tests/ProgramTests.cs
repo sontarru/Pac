f05d62f53dcf193b134931dadcc909a740f443a8
sontarru <dev@sontar.ru>
2025-08-04 03:08:57 +0300

    Add the solution skeleton

---
+++ test/Sontar.Pac.Tests/ProgramTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Sontar.Pac;
+
+/// <summary>
+/// Integration tests.
+/// </summary>
+public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _webApplicationFactory;
+
+    public ProgramTests(WebApplicationFactory<Program> webApplicationFactory)
+    {
+        _webApplicationFactory = webApplicationFactory;
+    }
+
+    [Fact]
+    public async Task Get_Root_Returns_hello_world()
+    {
+        var client = _webApplicationFactory.CreateClient();
+        var response = await client.GetAsync("/");
+
+        var mediaType = response.Content.Headers.ContentType?.MediaType;
+        mediaType.Should().Be("text/plain");
+
+        var content = await response.Content.ReadAsStringAsync();
+        content.Should().Be("Hello World!");
+    }
+}
